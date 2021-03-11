bf9f577df5c79ff7945a2771edbd9fdf490cd8da
Matteo <grecomatteo86@gmail.com>
2021-03-11 16:11:09 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@ var barsMenu = $('a > .fa-bars');
 var hambLinks = $('.hamburger-menu');
 var close = $('.close');
 
+// 1. il menu compare al mio click e scompare al mio click
+
+/*
+barsMenu.click(
+
+  function () {
+    hambLinks.fadeIn('slow');
+  }
+
+);
+
+close.click(
+
+  function(){
+    hambLinks.fadeOut('fast');
+  }
+
+);
+*/
+
+
+// 2. il menu compare e scompare automaticamente ad un solo click, con effetto fadeIn fadeOut
+
 barsMenu.click(
 
   function () {
@@ -18,11 +41,3 @@ barsMenu.click(
   }
 
 );
-
-// close.click(
-//
-//   function(){
-//     hambLinks.fadeOut('fast');
-//   }
-//
-// );
