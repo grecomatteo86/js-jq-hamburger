5ebb8d1a718ed24fa8d497e969120e2f08145d5c
Matteo <grecomatteo86@gmail.com>
2021-03-11 15:46:07 +0100

    hide on click commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 var barsMenu = $('a > .fa-bars');
 var hambLinks = $('.hamburger-menu');
+var close = $('.close');
 
 barsMenu.click(
 
   function () {
     hambLinks.show('slow');
+  }
+
+);
+
+close.click(
+
+  function(){
+    hambLinks.hide('slow');
   }
 
 );
